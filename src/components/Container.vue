4402cf6fe7ae39e94c1ff3bcd3868704f0110d36
silaslowe@gmail.com <silaslowe@gmail.com>
2021-07-25 19:38:05 -0500

    container werks

---
+++ src/components/Container.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center align-center">
+    <div
+        class="mx-4 my-6 h-screen border-8 w-4/5 border-black flex justify-center align-center">
+      <div class="w-1/2 h-full flex justify-center align-center">
+        <Logo/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "@/components/Logo";
+
+export default {
+  name: "Container",
+  components: {Logo}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+      class="border border-black rounded-full h-24 w-24 flex items-center justify-center">
+    <h1>Silas Lowe</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo"
+}
+</script>
+
+<style scoped>
+
+</style>
