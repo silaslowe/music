6c984c8d3292ba2b58468fb6a58f97329c1df01c
silaslowe@gmail.com <silaslowe@gmail.com>
2021-08-07 22:01:59 -0500

    redeploy

--- src/components/NamePic.vue
+++ src/components/NamePic.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class=''>
-      <h1 class="m-10 text-3xl">SILAS LOWE</h1>
-      <img src="../assets/trashcan.jpeg" class="p-5 border shadow-md mb-10" alt="Silas Lowe">
+  <div class="m-6">
+      <div >
+      <iframe
+      class="w-26"
+      src="https://open.spotify.com/follow/1/?uri=spotify:artist:2VSAW7OcmNRphkIwRI3zQt&theme=basic"
+      width="300"
+      height="56"
+      scrolling="no"
+      frameborder="0"
+      style="border:none; overflow:hidden;"
+      allowtransparency="true"
+    />
+    </div>
+    <h1 class="text-3xl">SILAS LOWE</h1>
+    <h3 class="text-sm">MUSICIAN</h3>
   </div>
+  <img src="../assets/trashcan.jpeg" class="p-5 border shadow-md mb-10" alt="Silas Lowe" />
 </template>
+
+<style scoped>
+    .count-num {
+        visibility: hidden;
+    }
+</style>
--- src/components/Spotify.vue
+++ src/components/Spotify.vue
@@ -1,5 +1,12 @@
 <template>
-    <div class="mb-10">
-    <iframe src="https://open.spotify.com/embed/artist/2VSAW7OcmNRphkIwRI3zQt" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-    </div>
+  <div class="mb-10 flex flex-col justify-center items-center">
+    <iframe
+      src="https://open.spotify.com/embed/artist/2VSAW7OcmNRphkIwRI3zQt"
+      width="300"
+      height="380"
+      frameborder="0"
+      allowtransparency="true"
+      allow="encrypted-media"
+    ></iframe>
+  </div>
 </template>
